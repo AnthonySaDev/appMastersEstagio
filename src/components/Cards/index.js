90324677d2a17ae6c08527d9da793aeac2cb9b63
AnthonySaDevMobile <anthonysareis11@gmail.com>
2023-07-10 14:56:43 -0300

    tentando hospedar

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -96,7 +96,7 @@ const Cards = ({ data }) => {
         </div>
       </div>
         <h1 className='text-center font-semibold my-5 hidden lg:flex items-center justify-center'>Place your cursor over the card 😉</h1>
-        <h1 className='text-center font-semibold my-3 lg:hidden'>Press the card 😉</h1>
+        <h1 className='text-center font-semibold my-3 lg:hidden'>Press the card 😉 </h1>
       <div>
         <div className="grid gap-20 justify-center items-center mt-10 w-9/12 mx-auto sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
           {isLoading
