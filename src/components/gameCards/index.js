907bb7ba85a319e08bf90af6b475ec3a1d69d0d5
Anthony Sá <129391343+AnthonySaDev@users.noreply.github.com>
2023-07-12 08:55:44 -0300

    Update paddin thumbnail

--- src/components/gameCards/index.js
+++ src/components/gameCards/index.js
@@ -47,7 +47,7 @@ export const GameCard = ({ item, filteredData, setVisible }) => {
           className="w-full h-full object-fill brightness-150 shadow-2xl"
         />
         <div className="absolute bg-transparent top-0 left-0 w-full h-full">
-          <div className="p-[0.88rem] flex flex-col justify-between h-full">
+          <div className="p-[0.86rem] flex flex-col justify-between h-full">
             <div className="w-full h-[30%] sm:h-[40%] md:h-[35%] lg:h-[45%] xl:h[50%] 2xl:h-[60%]">
               <motion.img
                 src={item.thumbnail}
