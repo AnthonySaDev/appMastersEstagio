e9d6fb6671d08559c9978527cc9176aefb8c1c09
Anthony Sá <129391343+AnthonySaDev@users.noreply.github.com>
2023-07-14 08:56:46 -0300

    Update index.js

--- src/components/gameCards/index.js
+++ src/components/gameCards/index.js
@@ -52,7 +52,7 @@
                 <motion.img
                   src={item.thumbnail}
                   alt={item.title}
-                  className="w-full pl-[0.71rem] pr-[0.862rem] pt-[0.93rem] h-full object-cover"
+                  className="w-full pl-[0.68rem] pr-[0.862rem] pt-[0.9rem] h-full object-cover"
                   variants={imageVariants}
                   initial="hidden"
                   animate="visible"
